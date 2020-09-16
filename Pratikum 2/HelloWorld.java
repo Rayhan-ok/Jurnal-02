152cb9b829340b77807d7ca4e7a3ac01060ba490
Rayhan-ok <71366965+Rayhan-ok@users.noreply.github.com>
2020-09-16 15:05:10 +0700

    HelloWorld.java

---
+++ Pratikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : M.RAYHAN BILHAQI");
+		System.out.println("Nim  : 6706200031");
+		System.out.println("Kelas: D3RPLA44-04");
+	}
+}
